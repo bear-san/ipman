37e4c4844eb808dbfe496b418d072f894b7b964f
Kentaro Abe <kentaro@beah.jp>
2025-01-11 03:07:56 +0900

    add: サーバー実装

--- internal/server/server.go
+++ internal/server/server.go
@@ -14,10 +14,54 @@ type IPManServer struct {
 	IPRepo *ip_repo.IPRepo
 }
 
-func (s *IPManServer) AssignAddress(context.Context, *grpc.AssignAddressRequest) (*grpc.AssignAddressResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AssignAddress not implemented")
+func (s *IPManServer) AssignAddress(_ context.Context, in *grpc.AssignAddressRequest) (*grpc.AssignAddressResponse, error) {
+	var addressType string
+	switch in.AddressType {
+	case grpc.AddressType_LOCAL:
+		addressType = ip_repo.IP_ADDRESS_TYPE_LOCAL
+	case grpc.AddressType_GLOBAL:
+		addressType = ip_repo.IP_ADDRESS_TYPE_GLOBAL
+	default:
+		return nil, status.Errorf(codes.InvalidArgument, "invalid Address Type: %s", in.GetAddressType().String())
+	}
+	assignedIPAddress, err := s.IPRepo.AssignIPAddress(addressType)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "failed to assign IP Address: %v", err.Error())
+	}
+	return &grpc.AssignAddressResponse{
+		Address: &grpc.IPAddress{
+			Address:           assignedIPAddress.Address,
+			Subnet:            assignedIPAddress.Subnet,
+			GatewayAddress:    assignedIPAddress.GatewayAddress,
+			AddressType:       in.AddressType,
+			Using:             assignedIPAddress.Using,
+			AutoAssignEnabled: assignedIPAddress.AutoAssignEnabled,
+			Description:       assignedIPAddress.Description,
+		},
+	}, nil
 }
 
-func (s *IPManServer) ReleaseAddress(context.Context, *grpc.ReleaseAddressRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ReleaseAddress not implemented")
+func (s *IPManServer) ReleaseAddress(_ context.Context, in *grpc.ReleaseAddressRequest) (*emptypb.Empty, error) {
+	var addressType string
+	switch in.Address.AddressType {
+	case grpc.AddressType_LOCAL:
+		addressType = ip_repo.IP_ADDRESS_TYPE_LOCAL
+	case grpc.AddressType_GLOBAL:
+		addressType = ip_repo.IP_ADDRESS_TYPE_GLOBAL
+	default:
+		return nil, status.Errorf(codes.InvalidArgument, "invalid Address Type: %s", in.Address.GetAddressType().String())
+	}
+	err := s.IPRepo.ReleaseIPAddress(ip_repo.IPAddress{
+		Address:           in.Address.Address,
+		Subnet:            in.Address.Subnet,
+		GatewayAddress:    in.Address.GatewayAddress,
+		AddressType:       addressType,
+		Using:             in.Address.Using,
+		AutoAssignEnabled: in.Address.AutoAssignEnabled,
+		Description:       in.Address.Description,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "failed to release IP Address: %v", err.Error())
+	}
+	return &emptypb.Empty{}, nil
 }
